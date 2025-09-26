e5cd41ae4991352109182ea2fd2b25816595d026
Nguyen Tuan An <anntse173412@fpt.edu.vn>
2025-09-26 22:03:23 +0700

    init

---
+++ packages/eslint-config/create-config.js
@@ -0,0 +1,41 @@
+import antfu from "@antfu/eslint-config"
+
+export default function createConfig(options, ...userConfigs) {
+  return antfu(
+    {
+      type: "app",
+      typescript: true,
+      formatters: true,
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "backtick",
+      },
+      ...options,
+    },
+    {
+      rules: {
+        "style/quotes": ["error", "double"],
+        "style/eol-last": "error",
+        "ts/consistent-type-definitions": ["error", "type"],
+        "no-console": ["warn"],
+        "antfu/no-top-level-await": ["off"],
+        // "node/no-process-env": ["error"],
+        "perfectionist/sort-imports": [
+          "error",
+          {
+            tsconfigRootDir: ".",
+          },
+        ],
+        "unicorn/filename-case": [
+          "error",
+          {
+            case: "kebabCase",
+            ignore: ["README.md"],
+          },
+        ],
+      },
+    },
+    ...userConfigs,
+  )
+}
