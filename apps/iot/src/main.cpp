626882aee4a0a697667976ad69b93cbae900914f
Nguyen Tuan An <tuanan298@icloud.com>
2025-10-15 22:40:01 +0700

    init commit

--- apps/iot/src/main.cpp
+++ apps/iot/src/main.cpp
@@ -7,6 +7,41 @@
 #include "StateMachine.h"
 #include "NetworkManager.h"
 #include "LEDStatusManager.h"
+#include <Wire.h>
+#include "Adafruit_PN532.h"
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define PN532_IRQ (4)
+#define PN532_RESET (5)
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+BLEServer *pServer = NULL;
+BLECharacteristic *pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+class MyServerCallbacks : public BLEServerCallbacks
+{
+  void onConnect(BLEServer *pServer)
+  {
+    deviceConnected = true;
+    Serial.println("Device connected");
+  };
+
+  void onDisconnect(BLEServer *pServer)
+  {
+    deviceConnected = false;
+    Serial.println("Device disconnected");
+  }
+};
 
 void setup()
 {
@@ -19,10 +54,49 @@ void setup()
   Global::bufferedLogger.reset(new BufferedLogger());
   Global::bufferedLogger->setTopic("esp/logs"); // temporary
   Global::logInfoLocal("Buffered logger initialized");
-  delay(5000);
+  Wire.begin(I2C_SDA, I2C_SCL);
+  nfc.begin();
+  Serial.println("Hello from ESP32!");
 
+  delay(5000);
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (!versiondata)
+  {
+    Serial.print("Didn't find PN53x board");
+    Serial.print("Check your wiring and the DIP switches!");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+  Serial.println("Found PN532 Board!");
+  Serial.print("Firmware ver. ");
+  Serial.print((versiondata >> 16) & 0xFF, DEC);
+  Serial.print('.');
+  Serial.println((versiondata >> 8) & 0xFF, DEC);
+  nfc.SAMConfig();
+
+  Serial.println("\nWaiting for an NFC Card...");
   currentState = STATE_INIT;
   Global::ledStatusManager->setStatus(currentState);
+
+  BLEDevice::init("MeBike-ESP32");
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID,
+      BLECharacteristic::PROPERTY_READ |
+          BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristic->setValue("Hello from MeBike ESP32");
+  pService->start();
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06); // iphne
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("BLE advertising started");
 
   AppConfig config = loadConfig();
   Global::networkManager.reset(new NetworkManager());
@@ -45,7 +119,39 @@ void setup()
 
 void loop()
 {
- 
+  uint8_t success;
+  uint8_t uid[] = {0, 0, 0, 0, 0, 0, 0}; // Buffer to store the card's UID 4 bytes th
+  uint8_t uidLength;                   
+
+
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  if (success)
+  {
+    Serial.println("Found a card!");
+    Serial.print("UID Length: ");
+    Serial.print(uidLength, DEC);
+    Serial.println(" bytes");
+    Serial.print("UID Value: ");
+    nfc.PrintHex(uid, uidLength); 
+    Serial.println("");
+
+    delay(1000);
+  };
+
+  if (deviceConnected && !oldDeviceConnected)
+  {
+    oldDeviceConnected = deviceConnected;
+    Serial.println("Device connected, starting notification");
+  }
+  if (!deviceConnected && oldDeviceConnected)
+  {
+    delay(500);                  
+    pServer->startAdvertising(); 
+    Serial.println("Start advertising");
+    oldDeviceConnected = deviceConnected;
+  }
+
   if (Global::ledStatusManager)
   {
     Global::ledStatusManager->update();
@@ -99,7 +205,6 @@ void loop()
     Log.info("State changed to %s (%d)\n", getStateName(currentState), currentState);
     Global::logInfoMQTT("State changed to %s (%d)", getStateName(currentState), currentState);
 
-   
     if (Global::ledStatusManager)
     {
       Global::ledStatusManager->setStatus(currentState);
