738009329321a72a5aa43cb0046c9cd053431421
trananhvy9420 <trananhvy9420@gmail.com>
2025-10-15 17:19:30 +0700

    fix(bike): update queryKey in useGetBikeByIDAllQuery for correct data retrieval

--- apps/frontend/src/hooks/query/Bike/useGetBIkeByIDAll.ts
+++ apps/frontend/src/hooks/query/Bike/useGetBIkeByIDAll.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { bikeService } from "@/services/bikeService";
 export const useGetBikeByIDAllQuery = (id: string) => {
     return useQuery({
-        queryKey: ["bikes", "history", id],
+        queryKey: ["bikes", "detail", id],
         queryFn: () => bikeService.getBikeByIdForAll(id),
     })
 }
