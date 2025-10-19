c894dcf77a058655ea4bf461b01c0171882de5b4
trananhvy9420 <trananhvy9420@gmail.com>
2025-10-19 14:24:01 +0700

    feat: Add wallet service and query hooks for managing user wallet data

---
+++ apps/mobile/hooks/query/Wallet/useGetMyWalletQuery.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { walletService } from "@/services/walletService";
+import type { MyWallet } from "@/services/walletService";
+export const fetchMyWallet = async (): Promise<MyWallet> => {
+  const response = await walletService.getMyWallet();
+  if(response.status === 200){
+    return response.data;
+  }
+  throw new Error("Failed to fetch user profile");
+}
+export const useGetMyWalletQuery = () => {
+    return useQuery({
+      queryKey: ["my-wallet"],
+      queryFn: () => fetchMyWallet(),
+      staleTime: 5 * 60 * 1000,
+    });
+}
---
+++ apps/mobile/services/walletService.ts
@@ -0,0 +1,40 @@
+import fetchHttpClient from "@lib/httpClient";
+import type { AxiosResponse } from "axios";
+export interface MyWallet {
+  _id: string;
+  user_id: string;
+  balance: {
+    $numberDecimal : string;
+  };
+  status : "ĐANG HOẠT ĐỘNG" | "KHÓA";
+  created_at : string;
+  updated_at : string;
+}
+const WALLET_BASE = "/wallets";
+const WALLET_ENDPOINTS = {
+  BASE: WALLET_BASE,
+  MY_WALLET: `${WALLET_BASE}/my-wallet`,
+  TOP_UP: `${WALLET_BASE}/increase`,
+  DEBIT: `${WALLET_BASE}/decrease`,
+} as const;
+import type { TopUpSchemaFormData , DecreaseSchemaFormData } from "@schemas/walletSchema";
+export const walletService = {
+  getMyWallet: async (): Promise<AxiosResponse<MyWallet>> => {
+    const response = await fetchHttpClient.get<MyWallet>(
+      WALLET_ENDPOINTS.MY_WALLET
+    );
+    return response;
+  },
+  topUpWallet: async (
+    data: TopUpSchemaFormData
+  ): Promise<AxiosResponse<MyWallet>> => {
+    const response = await fetchHttpClient.put<MyWallet>(WALLET_ENDPOINTS.TOP_UP, data);
+    return response;
+  },
+  debitWallet: async (
+    data: DecreaseSchemaFormData
+  ): Promise<AxiosResponse<MyWallet>> => {
+    const response = await fetchHttpClient.put<MyWallet>(WALLET_ENDPOINTS.DEBIT, data);
+    return response;
+  },
+};
