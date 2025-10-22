5f62651e931cb1ae89e252f1b01efe41a22e01c0
Nguyen Tuan An <tuanan298@icloud.com>
2025-10-22 13:37:47 +0700

    quên revert cái này commit nhầm

--- apps/iot/src/handlers/CommandHandler.cpp
+++ apps/iot/src/handlers/CommandHandler.cpp
@@ -166,7 +166,7 @@ void CommandHandler::handleBookingCommand(const char *command)
 
             if (Global::mqttManager)
             {
-                Global::mqttManager->publish(Global::getTopics().statusTopic.c_str(), "claimed", false);
+                Global::mqttManager->publish(Global::getTopics().commandBookingTopic.c_str(), "claimed", false); 
             }
             Global::logInfoMQTT("Booking command: claim");
         }
