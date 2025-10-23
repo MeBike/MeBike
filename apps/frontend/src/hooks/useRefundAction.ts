f5f86f921443b085e49244a873c87ca57e70b019
trananhvy9420 <trananhvy9420@gmail.com>
2025-10-24 00:44:03 +0700

    feat: create useRefundAction hook for managing refund requests

---
+++ apps/frontend/src/hooks/useRefundAction.ts
@@ -0,0 +1,18 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useGetAllRefundRequestQuery } from "./query/Refund/useGetAllRefundRequestQuery";
+import { RefundStatus } from "@/types";
+export const useRefundAction = ({ page, limit, status , hasToken}: {
+    hasToken?: boolean,
+    page?: number,
+    limit?: number,
+    status?: RefundStatus
+}) => {
+    const queryClient = useQueryClient();
+    const { data, isLoading, isError , refetch} = useGetAllRefundRequestQuery({ page, limit, status });
+    return {
+        data,
+        isLoading,
+        isError,
+        refetch
+    };
+};
