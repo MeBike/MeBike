630b39e291736fb3380b099538ebef0b7de71d50
Truongle68 <truonglephuc11@gmail.com>
2025-11-10 23:41:33 +0700

    refactor subscription package price

--- apps/backend/src/constants/subscription-packages.ts
+++ apps/backend/src/constants/subscription-packages.ts
@@ -3,7 +3,7 @@ import { SubscriptionPackage } from './enums'
 export const PACKAGE_CONFIG: Record<SubscriptionPackage, { price: string; max_usages: number | null }> =
   {
     [SubscriptionPackage.BASIC]: {
-      price: '99000',
+      price: '119000',
       max_usages: 30
     },
     [SubscriptionPackage.PREMIUM]: {
