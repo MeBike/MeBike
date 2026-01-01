a668fef1f78bd6e7c3d2f1fa47886bfdd87c7858
trananhvy9420 <trananhvy9420@gmail.com>
2026-01-01 21:41:57 +0700

    refactor(create-user): remove unused useNavigate import

--- apps/frontend/src/app/admin/customers/components/create-user.tsx
+++ apps/frontend/src/app/admin/customers/components/create-user.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { PageHeader } from "@/components/PageHeader";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
