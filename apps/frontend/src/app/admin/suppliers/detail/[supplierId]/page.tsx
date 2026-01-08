7d823d7b65ea1e5ce4f1f55641d99ca3b744a0cd
trananhvy2003 <trananhvy9420@gmail.com>
2026-01-08 08:08:42 +0700

    refactor: extract SupplierDetailClient and integrate into page.ts

--- apps/frontend/src/app/admin/suppliers/detail/[supplierId]/page.tsx
+++ apps/frontend/src/app/admin/suppliers/detail/[supplierId]/page.tsx
@@ -1,46 +1,6 @@
-"use client"
-import SupplierDetail from "./SupplierDetail";
-import React from "react";
-import { useSupplierActions } from "@/hooks/use-supplier";
-export default function Page({
-  params,
-}: {
-  params: Promise<{ supplierId: string }>;
-}) {
-  const { supplierId } = React.use(params);
-  const { detailSupplier, getUpdateSupplier , isLoadingDetailSupplier} = useSupplierActions({
-    hasToken: true,
-    supplier_id: supplierId,
-  });
-
-  const handleUpdateSupplier = async ({name , address , contactFee , phone } : {
-    name?: string;
-    address?: string;
-    contactFee?: number;
-    phone?: string;
-  }) => {
-    try {
-       getUpdateSupplier({
-        data: { 
-          name : name,
-          address : address,
-          contactFee : contactFee,
-          phone : phone
-         },
-        id: supplierId,
-      });
-    } catch (error) {
-      console.error("Failed to update supplier:", error);
-    }
-  };  
-  if (isLoadingDetailSupplier) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <SupplierDetail
-      supplier={detailSupplier || null}
-      onSubmit={handleUpdateSupplier}
-    />
-  );
+import SupplierDetailClient from "./SupplierDetailClient";
+const Page = ({ params }: { params: Promise<{ supplierId: string }> }) => {
+    return (
+        <SupplierDetailClient params={params}/>
+    )
 }
