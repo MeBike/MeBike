a24e2e1b99fa532aed0e3bc087f896b0294b1774
trananhvy9420 <trananhvy9420@gmail.com>
2026-02-20 22:09:52 +0700

    feat(errors): add station error message mapping utility

--- apps/frontend/src/hooks/use-station.ts
+++ apps/frontend/src/hooks/use-station.ts
@@ -14,6 +14,7 @@ import { useGetStationRevenue } from "./query/Station/useGetStationRevenue";
 import { useGetNearestAvailableBike } from "./query/Station/useGetNearestAvailableBike";
 import { QUERY_KEYS } from "@constants/queryKey";
 import getErrorMessage from "@/utils/error-message";
+import { getErrorMessageFromStationCode } from "@/utils/map-message";
 interface StationActionProps {
   hasToken?: boolean;
   stationId?: string;
