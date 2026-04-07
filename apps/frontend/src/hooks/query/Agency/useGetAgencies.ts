b4440f35101c28328e352344433bf76f736af8ae
trananhvy9420 <trananhvy9420@gmail.com>
2026-04-07 11:55:17 +0700

    feat(agencies): add hooks for agency data retrieval
    - Introduced `useGetAgencies` hook for fetching agency data with pagination support.
    - Created `useGetDetailAgency` hook for detailed agency information retrieval.
    - Added new service index file for agency-related services.

---
+++ apps/frontend/src/hooks/query/Agency/useGetAgencies.ts
@@ -0,0 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
+import { agencyService } from "@/services/agency.service";
+import { HTTP_STATUS } from "@constants";
+const getAgencies = async ({
+  page,
+  pageSize,
+}: {
+  page?: number;
+  pageSize?: number;
+}) => {
+  try {
+    const response = await agencyService.getAgencies({
+      page: page,
+      pageSize: pageSize,
+    });
+    if (response.status === HTTP_STATUS.OK) {
+        return response.data
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to fetch agencies");
+  }
+};
+export const useGetAgencies = ({page,pageSize}:{page?:number,pageSize?:number}) => {
+    return useQuery({
+        queryKey:["data","agencies"],
+        queryFn : () => getAgencies({page:page,pageSize:pageSize}),
+    })
+}
