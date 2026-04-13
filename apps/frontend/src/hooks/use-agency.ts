425ab2297bcec1344a508e3dcf198c24a1017140
trananhvy9420 <trananhvy9420@gmail.com>
2026-04-13 12:02:58 +0700

    refactor(hooks): integrate agency request operations into useAgencyActions
    Extend the useAgencyActions hook to incorporate query and mutation hooks for managing agency requests, including registration, approval, rejection, cancellation, and detail fetching. Introduce new callback functions for these operations and ensure proper error handling and query invalidation. Adjust imports and parameters to support the expanded functionality.

--- apps/frontend/src/hooks/use-agency.ts
+++ apps/frontend/src/hooks/use-agency.ts
@@ -1,32 +1,73 @@
-import { useGetAgencies, useGetAgencyDetail, useGetAgencyStat } from "@queries";
+import {
+  useGetAgencies,
+  useGetAgencyDetail,
+  useGetAgencyStat,
+  useGetAgencyRequests,
+  useGetAgencyRequestDetail,
+} from "@queries";
 import {
   useUpdateAgencyStatusMutation,
   useUpdateAgencyMutation,
+  useApproveAgencyRequestMutation,
+  useCancelAgencyRequestMutation,
+  useRegisterAgencyRequestMutation,
+  useRejectAgencyRequestMutation,
 } from "@mutations";
 import { toast } from "sonner";
 import { useCallback } from "react";
 import { useRouter } from "next/navigation";
-import { UpdateAgencyFormData, UpdateAgencyStatusFormData } from "@/schemas";
+import {
+  UpdateAgencyFormData,
+  UpdateAgencyStatusFormData,
+  RegisterAgencyFormData,
+} from "@/schemas";
 import { HTTP_STATUS } from "@/constants";
 import { useQueryClient } from "@tanstack/react-query";
 import {
   getErrorMessageFromAgencyCode,
   getAxiosErrorCodeMessage,
 } from "@utils";
+import { id } from "date-fns/locale";
 export interface AgencyActionProps {
   hasToken?: boolean;
   agency_id?: string;
   page?: number;
   pageSize?: number;
+  agency_request_id?:string;
 }
 export const useAgencyActions = ({
   hasToken,
   agency_id,
   page,
   pageSize,
+  agency_request_id
 }: AgencyActionProps) => {
   const router = useRouter();
   const queryClient = useQueryClient();
+  const {
+    data: agencyRequest,
+    refetch: refetchGetAgencyRequest,
+    isLoading: isLoadingAgencyRequest,
+  } = useGetAgencyRequests({ page: page, pageSize: pageSize });
+  const getAgencyRequest = useCallback(() => {
+    if (!hasToken) {
+      router.push("/login");
+      return;
+    }
+    refetchGetAgencies();
+  }, [refetchGetAgencyRequest, hasToken, router, page]);
+    const {
+    data: agencyRequestDetail,
+    refetch: refetchGetAgencyRequestDetail,
+    isLoading: isLoadingAgencyRequestDetail,
+  } = useGetAgencyRequestDetail({ id:agency_request_id || ""});
+  const getAgencyRequestDetail = useCallback(() => {
+    if (!hasToken) {
+      router.push("/login");
+      return;
+    }
+    refetchGetAgencyRequestDetail();
+  }, [refetchGetAgencyRequestDetail, hasToken, router, page]);
   const {
     data: agencies,
     refetch: refetchGetAgencies,
@@ -65,6 +106,10 @@ export const useAgencyActions = ({
   }, [refetchAgencyStats, hasToken, router, agency_id]);
   const updateAgencyMutation = useUpdateAgencyMutation();
   const updateStatusMutation = useUpdateAgencyStatusMutation();
+  const approveAgencyRequestMutation = useApproveAgencyRequestMutation();
+  const rejectAgencyRequestMutation = useRejectAgencyRequestMutation();
+  const cancelAgencyRequestMutation = useCancelAgencyRequestMutation();
+  const registerAgencyRequestMutation = useRegisterAgencyRequestMutation();
   const updateAgency = useCallback(
     async (data: UpdateAgencyFormData, id: string) => {
       if (!hasToken) {
@@ -81,8 +126,8 @@ export const useAgencyActions = ({
           queryClient.invalidateQueries({
             queryKey: ["data", "agencies"],
           });
-           queryClient.invalidateQueries({
-            queryKey: ["stats", "agency",id],
+          queryClient.invalidateQueries({
+            queryKey: ["stats", "agency", id],
           });
         }
       } catch (error) {
@@ -91,7 +136,7 @@ export const useAgencyActions = ({
         throw error;
       }
     },
-    [],
+    [hasToken, updateAgencyMutation],
   );
   const updateAgencyStatus = useCallback(
     async (data: UpdateAgencyStatusFormData, id: string) => {
@@ -109,8 +154,8 @@ export const useAgencyActions = ({
           queryClient.invalidateQueries({
             queryKey: ["data", "agencies"],
           });
-           queryClient.invalidateQueries({
-            queryKey: ["stats", "agency",id],
+          queryClient.invalidateQueries({
+            queryKey: ["stats", "agency", id],
           });
         }
       } catch (error) {
@@ -119,7 +164,138 @@ export const useAgencyActions = ({
         throw error;
       }
     },
-    [],
+    [updateStatusMutation, hasToken, id],
+  );
+  const approveAgencyRequest = useCallback(
+    async ({ id, description }: { id: string; description?: string }) => {
+      if (!hasToken) {
+        router.push("/login");
+        return;
+      }
+      try {
+        const result = await approveAgencyRequestMutation.mutateAsync({
+          id: id,
+          description: description,
+        });
+        if (result.status === HTTP_STATUS.OK) {
+          toast.success("Chấp nhận yêu cầu trở thành Agency");
+          queryClient.invalidateQueries({
+            queryKey: [],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["data", "agencies"],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["stats", "agency", id],
+          });
+        }
+      } catch (error) {
+        const error_code = getAxiosErrorCodeMessage(error);
+        toast.error(getErrorMessageFromAgencyCode(error_code));
+        throw error;
+      }
+    },
+    [approveAgencyRequestMutation, hasToken],
+  );
+  const rejectAgencyRequest = useCallback(
+    async ({
+      id,
+      reason,
+      description,
+    }: {
+      id: string;
+      reason?: string;
+      description?: string;
+    }) => {
+      if (!hasToken) {
+        router.push("/login");
+        return;
+      }
+      try {
+        const result = await rejectAgencyRequestMutation.mutateAsync({
+          id: id,
+          description: description,
+          reason: reason,
+        });
+        if (result.status === HTTP_STATUS.OK) {
+          toast.success("Từ chối yêu cầu trở thành Agency");
+          queryClient.invalidateQueries({
+            queryKey: [],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["data", "agencies"],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["stats", "agency", id],
+          });
+        }
+      } catch (error) {
+        const error_code = getAxiosErrorCodeMessage(error);
+        toast.error(getErrorMessageFromAgencyCode(error_code));
+        throw error;
+      }
+    },
+    [rejectAgencyRequestMutation, hasToken],
+  );
+  const cancelAgencyRequest = useCallback(
+    async ({ id }: { id: string }) => {
+      if (!hasToken) {
+        router.push("/login");
+        return;
+      }
+      try {
+        const result = await cancelAgencyRequestMutation.mutateAsync({
+          id: id,
+        });
+        if (result.status === HTTP_STATUS.OK) {
+          toast.success("Hủy đơn trở thành Agency");
+          queryClient.invalidateQueries({
+            queryKey: [],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["data", "agencies"],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["stats", "agency", id],
+          });
+        }
+      } catch (error) {
+        const error_code = getAxiosErrorCodeMessage(error);
+        toast.error(getErrorMessageFromAgencyCode(error_code));
+        throw error;
+      }
+    },
+    [rejectAgencyRequestMutation, hasToken],
+  );
+  const registerAgencyRequest = useCallback(
+    async ({ data }: { data: RegisterAgencyFormData }) => {
+      if (!hasToken) {
+        router.push("/login");
+        return;
+      }
+      try {
+        const result = await registerAgencyRequestMutation.mutateAsync({
+          data: data,
+        });
+        if (result.status === HTTP_STATUS.OK) {
+          toast.success("Đăng ký trở thành Agency thành công");
+          queryClient.invalidateQueries({
+            queryKey: [],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["data", "agencies"],
+          });
+          queryClient.invalidateQueries({
+            queryKey: ["stats", "agency", id],
+          });
+        }
+      } catch (error) {
+        const error_code = getAxiosErrorCodeMessage(error);
+        toast.error(getErrorMessageFromAgencyCode(error_code));
+        throw error;
+      }
+    },
+    [rejectAgencyRequestMutation, hasToken],
   );
   return {
     agencies,
@@ -133,5 +309,15 @@ export const useAgencyActions = ({
     isLoadingAgencyStats,
     updateAgency,
     updateAgencyStatus,
+    approveAgencyRequest,
+    rejectAgencyRequest,
+    cancelAgencyRequest,
+    registerAgencyRequest,
+    agencyRequest,
+    getAgencyRequest,
+    isLoadingAgencyRequest,
+    getAgencyRequestDetail,
+    agencyRequestDetail,
+    isLoadingAgencyRequestDetail
   };
 };
